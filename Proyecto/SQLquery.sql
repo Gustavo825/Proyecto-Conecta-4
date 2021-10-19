cd83332ea1c0becee9079b5a52319ebe3c8f080f
Gustavo825 <41050062+Gustavo825@users.noreply.github.com>
2021-10-19 14:03:02 -0500

    SQL Query
    Tabla de jugadores

---
+++ Proyecto/SQLquery.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Jugadores] (
+        [JugadorId] [int] NOT NULL IDENTITY(1,1),
+        [usuario] [nvarchar](max),
+        [correo] [nvarchar](max),
+        [contrasenia] [nvarchar](max),
+        [puntaje][float](23),
+		Primary Key (JugadorId)
+    )
