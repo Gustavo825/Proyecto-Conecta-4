8aca1dac1ae2855046e9ade8d9664d76a39c8ab9
Gustavo825 <guxtavo825@gmail.com>
2021-10-25 00:50:08 -0500

    Usar este
    :)

---
+++ Proyecto/Juego/Chat/ChatJuego/Host/Host.cs
@@ -0,0 +1,24 @@
+﻿using ChatJuego.Base_de_datos;
+using ChatJuego.Servicios;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatJuego.Host
+{
+    class Host
+    {
+        static void Main(string[] args)
+        {
+            JugadorContexto jc = new JugadorContexto();
+            ServiceHost host = new ServiceHost(typeof(Servicio));
+            host.Open();
+            Console.WriteLine("Servidor corriendo");
+            Console.ReadLine();
+            host.Close();
+        }
+    }
+}
---
+++ Proyecto/Juego/Chat/ChatJuego/Servicios/IChatServicio.cs
@@ -0,0 +1,23 @@
+﻿using ChatJuego.Base_de_datos;
+using System.ServiceModel;
+
+namespace ChatJuego.Host
+{
+    [ServiceContract(CallbackContract = typeof(IJugadorCallBack))]
+    public interface IChatServicio
+    {
+
+        [OperationContract(IsOneWay = true)]
+        void inicializar();
+
+        
+        [OperationContract(IsOneWay = true)]
+        void mandarMensaje(Mensaje mensaje, Jugador jugadorQueMandaMensaje);
+
+        [OperationContract(IsOneWay = true)]
+        void mandarMensajePrivado(Mensaje mensaje, string nombreJugador, Jugador jugadorQueMandaMensaje);
+
+
+        
+    }
+}
